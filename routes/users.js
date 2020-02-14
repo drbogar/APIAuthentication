4ed94a4e99445bac7705f226be2f8c32144484c0
drbogar <drbogar@gmail.com>
2020-02-14 13:23:51 +0100

    202002141323

--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,17 @@ const passport = require('passport');
 const passportConf = require('../passport');
 
 const { validateBody, schemas} = require('../helpers/routeHelpers');
-const UsersController = require('../controllers/users')
+const UsersController = require('../controllers/users');
+const passportSignIn = passport.authenticate('local', { session: false });
+const passportJWT = passport.authenticate('jwt', { session: false });
 
 router.route('/signup')
     .post(validateBody(schemas.authSchema), UsersController.signUp);
 
 router.route('/signin')
-    .post(validateBody(schemas.authSchema), passport.authenticate('local', { session: false }), UsersController.signIn);
+    .post(validateBody(schemas.authSchema), passportSignIn, UsersController.signIn);
 
 router.route('/secret')
-    .get(passport.authenticate('jwt', { session: false }), UsersController.secret);
+    .get(passportJWT, UsersController.secret);
 
 module.exports = router;
