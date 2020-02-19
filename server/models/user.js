7b0e08c86fbae4c0f724b151fdec0158f735fa9c
drbogar <drbogar@gmail.com>
2020-02-19 13:31:05 +0100

    Add Google OAuth

--- server/models/user.js
+++ server/models/user.js
@@ -4,26 +4,52 @@ const Schema = mongoose.Schema;
 
 // Create a schema
 const userSchema = new Schema({
-    email: {
+    method: {
         type: String,
-        required: true,
-        unique: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
+        enum: ['local', 'google', 'facebook'],
         required: true
-    }
+    },
+    local: {
+        email: {
+            type: String,
+            lowercase: true
+        },
+        password: {
+            type: String
+        }
+    },
+    google: {
+        id: {
+            type: String
+        },
+        email: {
+            type: String,
+            lowercase: true
+        }
+    },
+    facebook: {
+        id: {
+            type: String
+        },
+        email: {
+            type: String,
+            lowercase: true
+        }
+    },
 });
 
 userSchema.pre('save', async function(next) {
     try {
+        if (this.method != 'local') {
+            next();
+        }
+
         // Generate a salt
         const salt = await bcrypt.genSalt(10);
         // Generate a password hash (salt + hash)
-        const passwordHash = await bcrypt.hash(this.password, salt);
+        const passwordHash = await bcrypt.hash(this.local.password, salt);
         // Re-assign hashed version over original, plain text password
-        this.password = passwordHash;
+        this.local.password = passwordHash;
         next();
     } catch (error) {
         next(error);
@@ -32,7 +58,9 @@ userSchema.pre('save', async function(next) {
 
 userSchema.methods.isValidPassword = async function(newPassword) {
     try {
-        return await bcrypt.compare(newPassword, this.password);
+        console.log('this.local.password', this.local.password);
+        console.log('newPassword', newPassword);
+        return await bcrypt.compare(newPassword, this.local.password);
     } catch (error) {
         throw new Error(error);
     }
--- server/passport.js
+++ server/passport.js
@@ -2,6 +2,7 @@ const passport  = require('passport');
 const jwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
 const LocalStrategy = require('passport-local').Strategy;
+const GooglePlusTokenStrategy = require('passport-google-plus-token');
 const { JWT_SECRET } = require('./configuration');
 const User = require('./models/user');
 
@@ -26,13 +27,49 @@ passport.use(new jwtStrategy({
     }
 }));
 
+// GOOGLE OAUTH STRATEGY
+passport.use('googleToken', new GooglePlusTokenStrategy({
+    clientID: '278195508427-q24vocl6bjvolnnh7dus7cqu0725ueih.apps.googleusercontent.com',
+    clientSecret: '6R6EjFbNWRYOgAF1SiT0V-yU'
+}, async (accessToken, refreshToken, profile, done) => {
+    try {
+        console.log('accessToken', accessToken);
+        console.log('refreshToken', refreshToken);
+        console.log('profile', profile);
+
+        // Check whether this current user exists in our DB
+        const existingUser = await User. findOne({ "google.id": profile.id });
+        if (existingUser) {
+            console.log('User already exists in our DB');
+            return done(null, existingUser);
+        }
+
+        console.log('User doesn\'t exists, we\'re creating a new one');
+
+        // If new account
+        const newUser = new User({
+            method: 'google',
+            google: {
+                id: profile.id,
+                email: profile.emails[0].value
+            }
+        });
+
+        await newUser.save();
+        done(null, newUser);
+    } catch (error) {
+        done(error, false, error.message);
+    }
+}));
+
 // LOCAL STRATEGY
 passport.use(new LocalStrategy({
     usernameField: 'email'
 }, async (email, password, done) => {
     try {
+        console.log('email', email);
         // Find the user given the email
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ "local.email": email });
 
         // If not, handle it
         if(!user) {
@@ -41,6 +78,8 @@ passport.use(new LocalStrategy({
 
         // Check if the password is correct
         const isMatch = await user.isValidPassword(password);
+        
+        console.log('isMatch', isMatch);
 
         // If not, handle it
         if (!isMatch) {
