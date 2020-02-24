e9516746fd15c40675954ae7fddce87421da1d5f
drbogar <drbogar@gmail.com>
2020-02-24 14:17:20 +0100

    React UI with Routing

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SignUP extends Component {
+    render() {
+        return (
+            <div>
+                This is a SignUp component
+            </div>
+        );
+    }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import Dashboard from './components/Dashboard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <BrowserRouter>
+        <App>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/dashboard" component={Dashboard} />
+        </App>
+    </BrowserRouter>,
+    document.querySelector('#root')
+);
 serviceWorker.unregister();
